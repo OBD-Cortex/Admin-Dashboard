7b9df95add685488b11e59f3f7bacccbfda80d8e
bodz <93529963+BodzTH@users.noreply.github.com>
2026-06-11 14:38:17 +0300

    Fix: correct ref type in TableFooter component to HTMLTableElement

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -37,8 +37,8 @@ const TableBody = React.forwardRef<
 TableBody.displayName = "TableBody"
 
 const TableFooter = React.forwardRef<
-  HTMLTableSectionElement,
-  React.HTMLAttributes<HTMLTableSectionElement>
+  HTMLTableElement,
+  React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
   <table
     ref={ref}
